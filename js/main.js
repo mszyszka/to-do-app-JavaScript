061b7a1f4dcd0b59bbd87bbd3d543e68ecedba18
Michal <mszyszka223@gmail.com>
2018-07-27 12:04:34 +0200

    I added some test JS and svg to button

---
+++ js/main.js
@@ -0,0 +1,12 @@
+
+
+//User click on the add button
+//If there is any text inside the item field, add that text to the todo list
+document.getElementById('add').addEventListener('click', function(){
+    var value = document.getElementById('item').value;
+
+    if (value){
+        
+    }
+
+});
