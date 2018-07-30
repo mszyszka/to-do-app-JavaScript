2f31ad2415a110a252b95ff2a62bf1127cd2ecf3
Michal <mszyszka223@gmail.com>
2018-07-30 13:34:55 +0200

    I added removeItem and completeItem functions

--- js/main.js
+++ js/main.js
@@ -9,9 +9,35 @@ var completeSVG = '<svg version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000
 document.getElementById('add').addEventListener('click', function(){
     var value = document.getElementById('item').value;
 
-    if (value) addItemTodo(value);
+    if (value) {
+        addItemTodo(value);
+        document.getElementById('item').value = '';
+
+    }
 
     });
+
+function removeItem(){
+
+    var item = this.parentNode.parentNode;
+    var parent = item.parentNode;
+
+    parent.removeChild(item);
+}
+
+function completeItem(){
+ 
+    var item = this.parentNode.parentNode;
+    var parent = item.parentNode;
+    var id = parent.id;
+
+    var target = (id === 'todo') ? document.getElementById('completed'):document.getElementById('todo');
+
+    parent.removeChild(item);
+    target.insertBefore(item, target.childNodes[0]);
+
+    
+}
 
 // add new item to todo list
 function addItemTodo(text){
@@ -27,16 +53,23 @@ function addItemTodo(text){
     var remove = document.createElement('button');
     remove.classList.add('remove');
     remove.innerHTML = removeSVG;
+    
+    //Add click event for removing the item
+    remove.addEventListener('click', removeItem);
 
     var complete = document.createElement('button');
     complete.classList.add('complete');
     complete.innerHTML = completeSVG;
 
+    //Add click event for complete the item
+    complete.addEventListener('click', completeItem);
+
+
     buttons.appendChild(remove);
     buttons.appendChild(complete);
     item.appendChild(buttons);
 
-    list.appendChild(item);
+    list.insertBefore(item, list.childNodes[0]);
 
 
     }
